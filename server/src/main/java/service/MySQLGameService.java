0c47c19f1ecf9ef54e9f46b169f859be5f76428f
Heidi <heidi.climber@gmail.com>
2026-03-09 15:17:00 -0600

    mysqlgameservice setup

---
+++ server/src/main/java/service/MySQLGameService.java
@@ -0,0 +1,85 @@
+package service;
+
+import dataaccess.*;
+import exception.ResponseException;
+import model.AuthData;
+import model.GameData;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class MySQLGameService {
+    public record ListGamesRequest(String authToken) {}
+    public record ListGamesResponse(ArrayList<GameData> games) {}
+    public record CreateGameRequest(String gameName) {}
+    public record CreateGameResponse(int gameID) {}
+    public record JoinGameRequest(String playerColor, String gameID) {}
+    public record JoinGameResponse() {}
+
+    private MySQLGameDAO gameDAO;
+    private MySQLAuthDAO authDAO;
+
+    public ListGamesResponse listGames(ListGamesRequest l) throws ResponseException {
+        AuthData auth = authDAO.findAuth(l.authToken);
+        if (auth == null) {
+            throw new ResponseException(401, "Error: Unauthorized");
+        }
+
+        ArrayList<GameData> game = gameDAO.listGames();
+        if (game == null) {
+            ArrayList<GameData> games = new ArrayList<>();
+            return new MySQLGameService.ListGamesResponse(games);
+        }
+
+        return new MySQLGameService.ListGamesResponse(game);
+    }
+
+    public CreateGameResponse createGame(CreateGameRequest c, String token) throws ResponseException {
+        AuthData auth = authDAO.findAuth(token);
+        if (auth == null) {
+            throw new ResponseException(401, "Error: Unauthorized");
+        } else if (c.gameName() == null) {
+            throw new ResponseException(400, "Error: bad request");
+        }
+        int gameID = gameDAO.createGame(c.gameName);
+        return new MySQLGameService.CreateGameResponse(gameID);
+    }
+
+    public JoinGameResponse joinGame(JoinGameRequest j, String token) throws ResponseException {
+        AuthData auth = authDAO.findAuth(token);
+        if (auth == null) {
+            throw new ResponseException(401, "Error: Unauthorized");
+        }
+
+        if (j.gameID == null) {
+            throw new ResponseException(400, "Error: bad request");
+        }
+
+        int gid = Integer.parseInt(j.gameID);
+        GameData game = gameDAO.getGame(gid);
+
+        if (game == null) {
+            throw new ResponseException(400, "Error: bad request");
+        }
+
+        String whiteUser = game.getWhite();
+        String blackUser = game.getBlack();
+        String user = auth.getUser();
+
+        if (Objects.equals(j.playerColor, "BLACK") && (blackUser == null)) {
+            GameDAO.updateGame(gid, whiteUser, user);
+            return new MySQLGameService.JoinGameResponse();
+        } else if (Objects.equals(j.playerColor, "WHITE") && (whiteUser == null)) {
+            GameDAO.updateGame(gid, user, blackUser);
+            return new MySQLGameService.JoinGameResponse();
+        } else if (!(Objects.equals(j.playerColor, "BLACK")) && !(Objects.equals(j.playerColor, "WHITE"))) {
+            throw new ResponseException(400, "Error: bad request");
+        } else {
+            throw new ResponseException(403, "Error: Already Taken");
+        }
+    }
+
+    public void clear() throws ResponseException {
+        gameDAO.clear();
+    }
+}
